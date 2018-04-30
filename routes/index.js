b34da518e92c748f8ec6a7feebe5a843b7f95d7b
Fobid <fobidbumz@naver.com>
2018-04-30 22:09:09 +0900

    Migrate promise to async await

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,49 @@ const models = require('../models');
 const express = require('express');
 const router = express.Router();
 
-var generateUserId = () => {
-    var userId = new Date().getTime();
+function generateUserId(userId) {
+    return models.User.find({
+        attributes: ['userId'],
+        where: {
+            userId: userId
+        }
+    });
+};
 
-    return new Promise((resolved, rejected) => {
-        models.User.findAll({
-            attributes: ['userId'],
+function createUser(user) {
+    console.log('createUser: ' + JSON.stringify(user));
+
+    return models.User.create({
+        email: user.email,
+        userId: user.userId,
+        password: user.password,
+        name: user.name
+    });
+};
+
+function createAccount(account) {
+    console.log('createAccount: ' + JSON.stringify(account));
+
+    return models.Account.create({
+        accessToken: account.accessToken,
+        userId: account.userId,
+        type: account.type
+    })
+};
+
+function getUser(userId) {
+    return models.User.find({
+        attributes: ['userId', 'name'],
+        include: [{
+            model: models.Account,
             where: {
                 userId: userId
             }
-        }).then(users => {
-            if(users.length > 0) {
-                checkUserIdDuplicated();
-            } else {
-                resolved(userId);
-            }
-        });
+        }]
     });
 }
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', async (req, res, next) => {
     var accessToken = req.body.accessToken;
     var userId = req.body.userId;
     var password = req.body.password;
@@ -29,78 +52,69 @@ router.post('/signup', (req, res, next) => {
     var name = req.body.name;
     var type = req.body.type;
 
-    new Promise((resolved, rejected) => {
-        if (typeof userId == 'undefined') {
-            // TODO: 이메일 중복 확인
-            // if (email.isDuplicated) {
-            //      throw err;
-            // } else {
-                generateUserId().then(results => {
-                    userId = results;
-                    type = 'travlog';
+    var user;
+    var account;
 
-                    new Promise((resolved, rejected) => {
-                        models.User.create({
-                            email: email,
-                            userId: userId,
-                            password: password,
-                            name: name
-                        }).then((user) => {
-                            models.Account.create({
-                                accessToken: 'accessToken',
-                                userId: userId,
-                                type: type
-                            }).then((account) => {
-                                var resUser = {
-                                    userId: user.userId,
-                                    name: user.name
-                                }
-                                var response = {
-                                    user: resUser,
-                                    accessToken: account.accessToken
-                                }
+    if (typeof userId == 'undefined') {
+        // TODO: 이메일 중복 확인
+        // if (email.isDuplicated) {
+        //      throw err;
+        // } else {
 
-                                resolved(response);
-                            });
-                        });
-                    }).then(results => {
-                        resolved(results);
-                    });
-                 });
-            // }
+        userId = new Date().getTime().toString();
+        var result = await generateUserId(userId);
+
+        if(result == null) {
+            console.log('generateUserId result is null');
         } else {
-            // if(sns.isExist) {
-                models.User.find({
-                    attributes: ['userId', 'name'],
-                    include: [{
-                        model: models.Account,
-                        where: {
-                            userId: userId
-                        }
-                    }]
-                }).then(user => {
-                    resolved({
-                        accessToken: user.Accounts[0].accessToken,
-                        user: {
-                            userId: user.userId,
-                            name: user.name
-                        }
-                   });
-                });
-            // } else {
-            // }
-
+            // generateUserId();
         }
-    }).then(results => {
-        console.log('results: '+JSON.stringify(results));
-        res.json(results);
-    });
-})
 
+        console.log('generated userId: ' + userId);
+
+        type = 'travlog';
+
+        user = await createUser({
+            email: email,
+            userId: userId,
+            password: password,
+            name: name
+        });
+
+        console.log('created user: ' + JSON.stringify(user));
+
+        account = await createAccount({
+            accessToken: 'accessToken',
+            userId: userId,
+            type: type
+        });
+
+        console.log('created account: ' + JSON.stringify(account));
+
+        // }
+    } else {
+        // if(sns.isExist) {
+        user = await getUser(userId);
+        console.log('selected user: ' + JSON.stringify(user));
+        account = user.Accounts[0];
+        // } else {
+        // }
+    }
+
+    console.log('user: ' + JSON.stringify(user));
+
+    res.json({
+        user: {
+            userId: user.userId,
+            name: user.name
+        },
+        accessToken: account.accessToken
+    });
+});
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' })
+    res.render('index', { title: 'Express' })
 })
 
 module.exports = router
