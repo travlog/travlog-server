3256cffe25f875cf123784469885a064e90455fc
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-06-03 21:25:30 +0900

    feat: #47 add test of find user

--- test/user.js
+++ test/user.js
@@ -5,22 +5,43 @@ const expect = require('chai').expect
 
 describe('UserTest', function() {
 
-    before(() => {
+    let User = null
+
+    before(function() {
         mongoose.Promise = Promise;
         const mongoUri = config.mongo.url
         mongoose.connect(mongoUri, { server: { socketOptions: { keepAlive: 1 } } })
         mongoose.connection.on('error', () => {
             throw new Error(`unable to connect to database: ${mongoUri}`)
         })
-    })
 
-    it('should be connect mongo', () => {
+        const UserSchema = new mongoose.Schema({
+            id: { type: Number },
+            userId: String,
+            password: String,
+            name: String,
+            username: String,
+            profilePicture: String,
+            isDrop: Boolean,
+            createdDate: Date,
+            updatedDate: Date,
+            dropDate: Date
+        });
+
+        User = mongoose.model('user', UserSchema)
+    });
+
+    it('should be connect mongo', function() {
         const connected = 2
         expect(mongoose.connection.readyState).to.be.equal(connected);
     })
 
-    after(() => {
-        console.log(mongoose.connection.readyState)
+    it('expect empty userlist', async () => {
+        const userList = await User.find({}, 'id userId name username profilePicture').lean().exec()
+        expect(userList).to.have.lengthOf(0)
+    })
+
+    after(function () {
         if ([1,2].includes(mongoose.connection.readyState)) {
             mongoose.disconnect()
         }
